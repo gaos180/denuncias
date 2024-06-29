30670e179dc090e93ef7c4dbf838b94a0f78f3eb
Gaos <gabriel.rhormazabal@gmail.com>
2024-06-29 18:34:16 -0400

    adding a modal in administracion

--- sistema_denuncias/denuncias_ciudadanas/blog/urls.py
+++ sistema_denuncias/denuncias_ciudadanas/blog/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from website.views import publicaciones, registro_denuncia, create_report, success, mapa, registar, login_web, obteniendo
+from website.views import registro_denuncia, mapa, registar, login_web, obteniendo, administracion
 from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,13 +9,13 @@ urlpatterns = [
     path("login/", login_web, name="login"),
     path("admin/", admin.site.urls),
     path("accounts/profile/", admin.site.urls),
-    path("publicaciones/", publicaciones),
     path('mapa/', mapa, name="mapa"),
     path('', mapa),
     path('register/', registar, name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('ingreso_denuncia/', registro_denuncia, name='registro_denuncia'),
     path('api/denuncias/', obteniendo, name='obteniendo'),
+    path('administracion/', administracion, name='administracion'),
 ]
 
 if settings.DEBUG:
