ab8c263cc55191a1eef0ab912f92536396b00348
Gaos <gabriel.rhormazabal@gmail.com>
2024-07-02 17:27:26 -0400

    se mejora la visualización de administración

--- sistema_denuncias/denuncias_ciudadanas/blog/urls.py
+++ sistema_denuncias/denuncias_ciudadanas/blog/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from website.views import registro_denuncia, administracion, mapa, registar, login_web, obteniendo, terminos_y_condiciones
+from website.views import registro_denuncia, administracion, mapa, registar, login_web, obteniendo, terminos_y_condiciones, base_admin, base_admin_denuncia, base_admin_usuario
 from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -17,6 +17,11 @@ urlpatterns = [
     path('api/denuncias/', obteniendo, name='obteniendo'),
     path('administracion/', administracion, name='administracion'),
     path('terminos-y-condiciones/', terminos_y_condiciones, name='terminos-y-condiciones'),
+
+    path('a', base_admin, name='panel_admin'),
+    path('lista/', base_admin_denuncia, name='panel_admin_denuncia'),
+    path('lista_user/', base_admin_usuario, name='panel_admin_user'),
+
 ]
 
 if settings.DEBUG:
